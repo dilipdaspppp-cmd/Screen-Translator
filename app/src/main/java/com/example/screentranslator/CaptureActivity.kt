cf88818fc4c472e0ea76fda9fb2d9f6c14433fe6
dilipdaspppp-cmd <dilipdaspppp@gmail.com>
2026-09-18 13:55:38 +0530

    Update app/src/main/java/com/example/screentranslator/CaptureActivity.kt

--- app/src/main/java/com/example/screentranslator/CaptureActivity.kt
+++ app/src/main/java/com/example/screentranslator/CaptureActivity.kt
@@ -1,26 +1,11 @@
 package com.example.screentranslator
 
 import android.app.Activity
-import android.content.Intent
-import android.media.projection.MediaProjectionManager
 import android.os.Bundle
 
 class CaptureActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val mpm = getSystemService(MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
-        startActivityForResult(mpm.createScreenCaptureIntent(), 1)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
-            val i = Intent(this, TranslationService::class.java)
-            i.action = "PROJECTION_RESULT"
-            i.putExtra("resultCode", resultCode)
-            i.putExtra("data", data)
-            startService(i)
-        }
         finish()
     }
 }
